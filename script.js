f6ceb725742bc70e0cd1d28b45e0350cbf61168a
hussainHakimi <hussainbayan614@gmail.com>
2022-06-15 16:39:56 +0430

    done

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const canvas = document.getElementById('color');
-const context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-context.strokeStyle = '#BADA55';
-context.lineJoin = 'round';
-context.lineCap = 'round';
-context.lineWidth = 50;
-context.globalCompositeOperation = 'multiply';
+    ctx.strokeStyle = '#BADA55';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 50;
+    ctx.globalCompositeOperation = 'multiply';
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-let hue = 0;
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+    let hue = 0;
 
-function draw(e) {
-  if(!isDrawing) return;
-  context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  context.beginPath();
-  // strat form
-  context.moveTo(lastX, lastY);
-  // end in
-  context.lineTo(e.offsetX, e.offsetY);
-  context.stroke();
+    function draw(e) {
+      if(!isDrawing) return;
+      ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+      ctx.beginPath();
+      // strat form
+      ctx.moveTo(lastX, lastY);
+      // end in
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
 
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-  hue++;
-  if(heu > 360){
-    heu = 0;
-  }
-}
-canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-}); 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+      hue++;
+      if(heu > 360){
+        heu = 0;
+      }
+    }
+    canvas.addEventListener('mousedown', (e) => {
+      isDrawing = true;
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    }); 
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
